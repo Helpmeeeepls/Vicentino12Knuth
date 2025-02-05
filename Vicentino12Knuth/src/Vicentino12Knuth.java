fb0d175ede27727027a2fdad0a6c686b911854d1
Helpmeeeepls <166249336+Helpmeeeepls@users.noreply.github.com>
2025-02-05 08:18:47 +0800

    deleted:    Vicentino12Knuth/src/App.java new file:   Vicentino12Knuth/src/Vicentino12Knuth.java

---
+++ Vicentino12Knuth/src/Vicentino12Knuth.java
@@ -0,0 +1,9 @@
+import java.util.Date;
+
+ public class Vicentino12Knuth {
+    public static void main(String[] args) {
+    Date date = new Date();
+    System.err.println("Izrafeel B. Vicentino");
+    System.out.println(date);
+    }
+}
