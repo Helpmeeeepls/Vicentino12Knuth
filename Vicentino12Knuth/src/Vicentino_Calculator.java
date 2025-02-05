ce22f3e93723383450827062ae88e7ce72bd2a64
Helpmeeeepls <166249336+Helpmeeeepls@users.noreply.github.com>
2025-02-05 21:58:55 +0800

    modified:   Vicentino12Knuth/src/Vicentino_Calculator.java

--- Vicentino12Knuth/src/Vicentino_Calculator.java
+++ Vicentino12Knuth/src/Vicentino_Calculator.java
@@ -2,22 +2,22 @@ import java.util.Scanner;
 
 public class Vicentino_Calculator {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("enter your first numebr here ");
-        double num1 = scanner.nextDouble();
-            
-        System.out.print("enter your sencon numebr here");
-        double num2 = scanner.nextDouble();
-            
-        double addition = num1 + num2;
-        double subtraction = num1 - num2;
-        double multiplication = num1 * num2;
-        double division = num1 / num2;
-            
-        System.out.println("addition: " + addition);
-        System.out.println("subtraction: " + subtraction);
-        System.out.println("mltiplication: " + multiplication);
-        System.out.println("division: " + division);
-        
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.print("enter your first numebr here ");
+            double num1 = scanner.nextDouble();
+                    
+            System.out.print("enter your sencon numebr here");
+            double num2 = scanner.nextDouble();
+                    
+            double addition = num1 + num2;
+            double subtraction = num1 - num2;
+            double multiplication = num1 * num2;
+            double division = num1 / num2;
+                    
+            System.out.println("addition: " + addition);
+            System.out.println("subtraction: " + subtraction);
+            System.out.println("mltiplication: " + multiplication);
+            System.out.println("division: " + division);
+        }
     }
 }
