33ea45f3e52abf059f5dd056b95f97be1a6a8654
Helpmeeeepls <166249336+Helpmeeeepls@users.noreply.github.com>
2025-04-11 22:05:11 +0800

    modified:   Vicentino12Knuth/src/StudentGradeChecker.java 	modified:   bin/StudentGradeChecker.class

--- Vicentino12Knuth/src/StudentGradeChecker.java
+++ Vicentino12Knuth/src/StudentGradeChecker.java
@@ -1,23 +1,20 @@
 import java.util.Scanner;
-
+ 
 public class StudentGradeChecker {
     public static void main(String[] args) {
-        try (Scanner scanner = new Scanner(System.in)) {
-            System.out.print("Enter your grade (A-F): ");
-            char grade = Character.toUpperCase(scanner.next().charAt(0));
-
-        String message = switch (grade) {
-            case 'A' -> "Excellent!";
-            case 'B' -> "Good job!";
-            case 'C' -> "You passed.";
-            case 'D' -> "Needs improvement.";
-            case 'F' -> "Failed.";
-            default -> "Invalid grade.";
-        };
-        System.out.println(message);
-
-            scanner.close();
-            }
+        Scanner scanner = new Scanner(System.in);
+ 
+        System.out.print("Enter your grade (A-F): ");
+        char grade = scanner.next().charAt(0);
+ 
+        switch (grade) {
+            case 'A': System.out.println("Excellent!"); break;
+            case 'B': System.out.println("Good job!"); break;
+            case 'C': System.out.println("You passed."); break;
+            case 'D': System.out.println("Needs improvement."); break;
+            case 'F': System.out.println("Failed."); break;
+            default: System.out.println("Invalid grade.");
         }
     }
+}
 
